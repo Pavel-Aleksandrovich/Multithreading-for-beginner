7c8e41f339bf0f08b84eb472a425b67da5bcfe60
Pavel Mishanin <pavelmishanin@mail.ru>
2021-11-25 18:16:56 +0700

    Add Progress View

--- Multithreading for beginner/HomeViewController/PictureViewController.swift
+++ Multithreading for beginner/HomeViewController/PictureViewController.swift
@@ -10,16 +10,39 @@ import Alamofire
 
 class PictureViewController: UIViewController {
     
+    private let progressView = UIProgressView()
+    private let progressLabel = UILabel()
     private let image = UIImageView()
     let imageUrl = "https://cdn.nwmgroups.hu/s/img/i/1603/20160318hiperorias-csillaghalmaz.jpg"
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        initProgressLabel()
+        startLoader()
+        loadImage(imageUrl: imageUrl)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "PictureViewController"
         view.backgroundColor = .green
         initImage()
-        loadImage(imageUrl: imageUrl)
+        
     }
+    private func initProgressLabel() {
+        progressLabel.frame = CGRect(x: 0, y: 0, width: 300, height: 30)
+        view.addSubview(progressLabel)
+        progressLabel.center = view.center
+    }
+    
+    private func startLoader() {
+        progressView.frame = CGRect(x: 0, y: 0, width: 300, height: 30)
+        view.addSubview(progressView)
+        progressView.center = view.center
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
     private func initImage() {
         image.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
         view.addSubview(image)
@@ -27,12 +50,20 @@ class PictureViewController: UIViewController {
     }
     
     private func loadImage(imageUrl: String) {
-        AF.download(imageUrl).responseData { (response) in
-            if let data = response.value {
-                let imageData = UIImage(data: data)
-                self.image.image = imageData
+        AF.download(imageUrl)
+            .downloadProgress { progress in
+                self.progressLabel.text = progress.localizedDescription
+                self.progressView.setProgress(Float(progress.fractionCompleted), animated: true)
             }
-        }
+            
+            .responseData { (response) in
+                if let data = response.value {
+                    let imageData = UIImage(data: data)
+                    self.image.image = imageData
+                }
+                self.progressView.isHidden = true
+                self.progressLabel.isHidden = true
+            }
     }
 }
 
