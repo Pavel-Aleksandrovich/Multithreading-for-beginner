1b3514dd122a85f20852c0910260f83bdec241e0
Pavel Mishanin <pavelmishanin@mail.ru>
2021-11-30 13:46:47 +0700

    Create cell in code Loading image into table view cell

--- Multithreading for beginner/ButtonsViewController/TwoViewController.swift
+++ Multithreading for beginner/ButtonsViewController/TwoViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 class TwoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     private let tableView = UITableView()
-    private let imageArray:[String] = ["1", "2", "3"]
+    private let imageArray:[String] = ["https://i.artfile.ru/2560x1600_486677_[www.ArtFile.ru].jpg",
+         "https://i.artfile.ru/1920x1080_913850_[www.ArtFile.ru].jpg",
+         "https://www.culture.ru/storage/images/a4247ecb83d91aad3888641ceb86994b/c9dbf3637b6e163faa41aa3360dc06d9.jpeg"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +41,94 @@ class TwoViewController: UIViewController, UITableViewDataSource, UITableViewDel
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        return ImageTableViewCell.init(link: imageArray[indexPath.row])
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UIScreen.main.bounds.height
     }
     
 }
+
+class ImageTableViewCell: UITableViewCell {
+    
+    private let image = UIImageView()
+    private let link: String
+    
+    init(link: String) {
+        self.link = link
+        super.init(style: .default, reuseIdentifier: "cell")
+        
+        initialize()
+        downloadImage()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initialize() {
+        
+        image.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(image)
+        
+        NSLayoutConstraint.activate([
+            image.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            image.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            image.topAnchor.constraint(equalTo: contentView.topAnchor),
+            image.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+     private func downloadImage() {
+        let session = URLSession.shared
+        
+        guard let url = URL(string: link) else {return}
+        
+        session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.image.image = image
+                }
+            }
+        }.resume()
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
