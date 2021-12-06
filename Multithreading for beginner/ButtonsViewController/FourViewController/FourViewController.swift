e48e36468615a7f8277b0d253aba7d39a501febc
Pavel Mishanin <pavelmishanin@mail.ru>
2021-12-06 17:49:28 +0700

    Work with JSONDecoder

--- Multithreading for beginner/ButtonsViewController/FourViewController/FourViewController.swift
+++ Multithreading for beginner/ButtonsViewController/FourViewController/FourViewController.swift
@@ -9,10 +9,27 @@ import UIKit
 
 class FourViewController: UIViewController {
     
+     
+    private let shared = URLSession.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .purple
         title = "FourViewController"
+        
+        makeRequest()
+        
     }
+    
+    private func makeRequest() {
+        guard let url = URL(string: "https://v2.jokeapi.dev/joke/Any") else { return }
+        
+        shared.dataTask(with: url) { data, response, error in
+            if let data = data, let joke = try? JSONDecoder().decode(Joke.self, from: data) {
+                print(joke.setup)
+            }
+        }.resume()
+    }
+    
 }
